2afdc72fff1dd714e67d75c4f7831cab651e5b9f
H1MS3LF <h1ms3lf@outlook.com>
2014-08-13 18:49:56 -0400

    Minix Fix

---
+++ XBMCLauncher-master/README.md
@@ -0,0 +1,9 @@
+XBMCLauncher
+============
+
+A helper app to allow setting XBMC as your Launcher in Android
+without modifying the XBMC app.
+
+Based on a use-case that came up in the XBMC.org forums.
+
+[Play Store](https://play.google.com/store/apps/details?id=se.blunden.xbmclauncher)
---
+++ XBMCLauncher-master/src/se/blunden/xbmclauncher/MainActivity.java
@@ -0,0 +1,46 @@
+package se.blunden.xbmclauncher;
+
+import android.app.Activity;
+import android.content.ActivityNotFoundException;
+import android.content.ComponentName;
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends Activity {
+	
+	private String xbmcActivity = "hk.minix.xbmc/hk.minix.xbmc.Splash"; 
+	
+	private void launch()
+	{
+		Intent activityIntent;
+		
+		// Intent.makeMainActivity is only available in API level 11 and above
+		if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+			activityIntent = Intent.makeMainActivity(ComponentName.unflattenFromString(xbmcActivity));
+		} else {
+			activityIntent = new Intent(Intent.ACTION_MAIN);
+            activityIntent.setComponent(ComponentName.unflattenFromString(xbmcActivity));
+            activityIntent.addCategory(Intent.CATEGORY_LAUNCHER);
+		}
+		
+		try {
+			startActivity(activityIntent);
+			return;
+		}
+		catch (ActivityNotFoundException e)
+		{
+			Toast.makeText(this, "Activity " + xbmcActivity + " not found. Resetting default launcher preference...", Toast.LENGTH_SHORT).show();
+			
+			// Reset the default launcher preference to avoid getting stuck with a non-working launcher
+			getPackageManager().clearPackagePreferredActivities(getPackageName());
+		}
+	}
+
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		launch();
+		finish();
+	}
+}
